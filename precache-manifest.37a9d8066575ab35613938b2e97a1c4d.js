89b8bd2d2a7c47b267f6df23ca8f93350f901373
thepulkitgupta <thepulkitgupta@gmail.com>
2020-08-23 23:52:30 +0530

    .

---
+++ precache-manifest.37a9d8066575ab35613938b2e97a1c4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b9bfa4245168b23af3bed8c9be88cb",
+    "url": "/simple-react-app/index.html"
+  },
+  {
+    "revision": "58b7077d42ab6875e8e1",
+    "url": "/simple-react-app/static/js/2.3d3e1a40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-app/static/js/2.3d3e1a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "286276834e85add77ce3",
+    "url": "/simple-react-app/static/js/main.70dbaa09.chunk.js"
+  },
+  {
+    "revision": "d90462fd0856d83acee8",
+    "url": "/simple-react-app/static/js/runtime-main.5c9f111b.js"
+  }
+]);
