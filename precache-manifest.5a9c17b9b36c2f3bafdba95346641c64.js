e431b21bfe4ce2bbf1eb726a66b6f41d45e25b8b
thepulkitgupta <thepulkitgupta@gmail.com>
2020-08-24 00:04:01 +0530

    .

---
+++ precache-manifest.5a9c17b9b36c2f3bafdba95346641c64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4d9b215baf6f47591a0ab001671eb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58b7077d42ab6875e8e1",
+    "url": "/static/js/2.3d3e1a40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d3e1a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "286276834e85add77ce3",
+    "url": "/static/js/main.70dbaa09.chunk.js"
+  },
+  {
+    "revision": "32fc0017928fe73360ea",
+    "url": "/static/js/runtime-main.9ba19ff0.js"
+  }
+]);
